8bf584e7d5dedf6e6cfe64f3052799e90aae771f
kuliz <kennedynkulilishika@gmail.com>
2026-08-03 06:00:17 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-
-const supabaseUrl = 'https://clcfzeytfqrjngyemwiq.supabase.co';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
-
-// Declare and export in one go without redeclaring
-export const supabase = createClient(supabaseUrl, supabaseKey);
