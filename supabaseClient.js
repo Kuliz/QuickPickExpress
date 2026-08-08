9acd0e10c7c32777440871bc73e9b136f0129035
kuliz <kennedynkulilishika@gmail.com>
2026-08-08 17:28:49 +0200

    Add files via upload

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+// supabaseClient.js
+const SUPABASE_URL = 'https://clcfzeytfqrjngyemwiq.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_Ny1Y4E9jx5BuDYFJznKRZg_UGMOYM4_';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
